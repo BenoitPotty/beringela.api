46d1b47f362581951634e3139fe770c1e778cec6
Benoît Potty <benoit@potty.be>
2020-11-06 22:01:25 +0100

    Add StartupExtensions

--- Beringela.Api/Startup.cs
+++ Beringela.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Beringela.Core.Extensions;
 
 namespace Beringela.Api
 {
@@ -26,6 +27,8 @@ namespace Beringela.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddBeringela();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,8 @@ namespace Beringela.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseBeringela();
 
             app.UseHttpsRedirection();
 
---
+++ Beringela.Core/Extensions/StartupExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Beringela.Core.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddBeringela(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+        }
+
+        public static void UseBeringela(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                // TODO get those strings from a Beringela Config section in app.settings.json
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+        }
+    }
+}
