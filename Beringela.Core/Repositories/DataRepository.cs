fe10ec6cc58bbd455d82d9b3467d09d2fc610c9d
Benoît Potty <benoit@potty.be>
2020-11-08 10:47:46 +0100

    Add JSON Patch Support

--- Beringela.Core/Repositories/DataRepository.cs
+++ Beringela.Core/Repositories/DataRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Beringela.Core.Entities;
 using Beringela.Core.Exceptions;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Beringela.Core.Repositories
