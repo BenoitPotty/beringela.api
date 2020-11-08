676a279598db58b77880fba27068a6c25b3ed273
Benoît Potty <benoit@potty.be>
2020-11-08 10:45:39 +0100

    Add Health Controller

---
+++ Beringela.Core/Entities/HealthData.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Beringela.Core.Entities
+{
+    public class HealthData
+    {
+        public HealthData()
+        {
+            ApiName = "TODO";
+#if DEBUG
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+            BeringelaVersion = assemblyName.Version?.ToString();
+#endif
+        }
+        public string ApiName { get; set; }
+        public string ApiVersion { get; set; }
+        public string BeringelaVersion { get; set; }
+    }
+}
---
+++ Beringela.Core/Mvc/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Beringela.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beringela.Core.Mvc
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [AllowAnonymous, HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new HealthData());
+        }
+    }
+}
